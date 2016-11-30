4ddc62457f880d66e7b59f4ddd1f17c2a4b2a89c
Yuri Danilov <yuri@sensiedge.com>
2016-11-30 11:22:58 +0200

    Added BroadCast receiver in MainActivity to receive credentials over broadcast Added Logcat debug printouts, MainActivity EditTexts defined as members

--- app/src/main/java/io/cloudthing/sim/MainActivity.java
+++ app/src/main/java/io/cloudthing/sim/MainActivity.java
@@ -1,9 +1,13 @@
 package io.cloudthing.sim;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -13,6 +17,8 @@ import com.android.volley.Response;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.logging.Logger;
+
 import io.cloudthing.sim.connectivity.http.HttpRequestQueue;
 import io.cloudthing.sim.connectivity.http.ValidationRequestFactory;
 import io.cloudthing.sim.utils.CredentialCache;
@@ -20,32 +26,45 @@ import io.cloudthing.sim.utils.CredentialCache;
 public class MainActivity extends AppCompatActivity {
     private Context ctx;
 
+    private static MainActivity mainActivityRunningInstance;
+    private EditText mDeviceIdText;
+    private EditText mTokenText;
+    private EditText mTenantText;
+
+    private CredentialsReceiver mReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ctx = this.getApplicationContext();
+        mReceiver = new CredentialsReceiver(new Handler());
+        registerReceiver(mReceiver, new IntentFilter("cloudthingio.intent.action.credentials"));
+        mainActivityRunningInstance = this;
+
+        mDeviceIdText = (EditText) findViewById(R.id.deviceId);
+        mTokenText = (EditText) findViewById(R.id.token);
+        mTenantText = (EditText) findViewById(R.id.tenant);
     }
 
     public void confirm(View view) {
-        final EditText deviceIdText = (EditText) findViewById(R.id.deviceId);
-        final EditText tokenText = (EditText) findViewById(R.id.token);
-        final EditText tenantText = (EditText) findViewById(R.id.tenant);
 
         ValidationRequestFactory validationRequestFactory = new ValidationRequestFactory(
                 ctx,
-                tenantText.getText().toString(),
-                deviceIdText.getText().toString(),
-                tokenText.getText().toString());
-
+                mTenantText.getText().toString(),
+                mDeviceIdText.getText().toString(),
+                mTokenText.getText().toString());
+        Log.d("appdbg", "short_name: " + mTenantText.getText().toString());
+        Log.d("appdbg", "device_id: " + mDeviceIdText.getText().toString());
+        Log.d("appdbg", "token: " + mTokenText.getText().toString());
         validationRequestFactory.setListener(new Response.Listener() {
             @Override
             public void onResponse(Object response) {
                 Intent intent = new Intent(ctx, SendDataActivity.class);
                 CredentialCache.getInstance().setCredentials(
-                        tenantText.getText().toString(),
-                        deviceIdText.getText().toString(),
-                        tokenText.getText().toString());
+                        mTenantText.getText().toString(),
+                        mDeviceIdText.getText().toString(),
+                        mTokenText.getText().toString());
                 startActivity(intent);
             }
         });
@@ -73,8 +92,41 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
             return;
         }
-
-
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public static class CredentialsReceiver extends BroadcastReceiver {
+        private final Handler handler;
+        public CredentialsReceiver(Handler handler) {
+            this.handler = handler;
+        }
+
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    getInstance().updateUI( intent.getStringExtra("token"),
+                                            intent.getStringExtra("tenant"),
+                                            intent.getStringExtra("deviceId"));
+                }
+            });
+        }
+    }
+
+    public void updateUI(final String strToken,
+                         final String strTenant,
+                         final String strDeviceId) {
+        MainActivity.this.runOnUiThread(new Runnable() {
+            public void run() {
+                mTenantText.setText(strTenant);
+                mTokenText.setText(strToken);
+                mDeviceIdText.setText(strDeviceId);
+            }
+        });
+    }
+
+    public static MainActivity getInstance() {
+        return mainActivityRunningInstance;
     }
 }
